01c7a6547257eb4c0d73dbdf1367af1b4e5e0d7b
nikolaymakhonin <nikolay.makhonin@gmail.com>
2015-06-26 20:10:44 +0700

    ReactTemplate: Add Bundle Config

---
+++ dot.Net/ASP.Net/MVC/TemplateProjects/ReactTemplate/ReactTemplate/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+namespace SchoolWebApi
+{
+    public class BundleConfig
+    {
+        // Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/vendors").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/react/react-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/bootstrap.css"));
+        }
+    }
+}
