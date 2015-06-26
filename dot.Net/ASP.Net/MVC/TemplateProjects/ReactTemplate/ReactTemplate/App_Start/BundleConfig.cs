98e1ec7926523c6d9db5ec7c091c55eb4d9830af
nikolaymakhonin <nikolay.makhonin@gmail.com>
2015-06-26 21:06:15 +0700

    ReactTemplate: Minimum application

--- dot.Net/ASP.Net/MVC/TemplateProjects/ReactTemplate/ReactTemplate/App_Start/BundleConfig.cs
+++ dot.Net/ASP.Net/MVC/TemplateProjects/ReactTemplate/ReactTemplate/App_Start/BundleConfig.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
-namespace SchoolWebApi
+namespace ReactTemplate
 {
     public class BundleConfig
     {
         // Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/vendors").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js/vendors").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js/react").Include(
                 "~/Scripts/react/react-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js/app").Include(
                 ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css/vendors").Include(
                  "~/Content/bootstrap.css"));
         }
     }
---
+++ dot.Net/ASP.Net/MVC/TemplateProjects/ReactTemplate/ReactTemplate/Scripts/app/app.jsx
@@ -0,0 +1,12 @@
+﻿var App = React.createClass({
+	render: function() {
+		return (
+			<div> React {this.props.name} </div>
+		);
+	}
+});
+
+React.render(
+	<App name="MyApp" />,
+	document.getElementById('container')
+);
